0cb872bf689a26cfb1ca6f3c9b953b4536360810
stantheman12-code <stanleylee12@gmail.com>
2023-04-10 02:53:15 +0930

    Commit changes - Final v2

--- TravelProject/travelapp/urls.py
+++ TravelProject/travelapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('bookings/', views.app, name='app'),
+    path('', views.home, name='app'),
 ]
--- TravelProject/travelapp/views.py
+++ TravelProject/travelapp/views.py
@@ -4,7 +4,3 @@ from django.http import HttpResponseRedirect
 
 def home(request):
     return render(request, 'home.html')
-
-# When user inputs "/bookings" as the URL, the view will redirect the user to the external booking chatbot URL
-def app(request):
-    return HttpResponseRedirect('https://d3lugqqos7gipo.cloudfront.net/')
